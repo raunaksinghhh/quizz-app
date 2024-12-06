7c37196f9c50d41150249ae97117ba9f183ecb9c
raunaksinghh <repeatandcode@gmail.com>
2024-12-06 16:26:54 +0530

    smol change

---
+++ src/page/Quection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Quection() {
+  return (
+    <div className="quection">
+        <h1>quections</h1>
+        
+
+
+
+
+    </div>
+  )
+}
+
+export default Quection
